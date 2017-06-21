3a79671d2e6d232c9b354620ae55f1368415944e
Colin May <satsukikorin@gmail.com>
2017-06-21 16:26:32 -0700

    Simplified promises
    Saved earlier "hand-rolled" promise code (in revised form) as a commented-out example, but for the main method, used simple plugin-supplied promise syntax.

--- services/books.js
+++ services/books.js
@@ -1,41 +1,67 @@
 var bookServiceEndpoint = "http://www.mocky.io/v2/593a06041100003004a9557c";
-
 var request = require('request-promise');
 
 module.exports = {
-    
+
     getByCategory: function(category) {
-        
+
         var options = {
             uri: bookServiceEndpoint,
-            headers: {
-                'User-Agent': 'Request-Promise'
-            },
             json: true
         };
 
-        var servicePromise = new Promise((resolve, reject) => {
-            
-            var fetch = request(options) || undefined;
-            
-            fetch.then(function(books){
-                
-                if ( books[category] ) {
-                    resolve( books[category] );
+        return request(options).then(function(books){
+
+                if ( books && books[category] ) {
+                    return books[category]
                 }
                 else {
-                    reject(
-                        { 
-                            error: "No Books Found"
-                        }
-                    );
+                    return {};
                 }
-            
-            });
-            
-        });
-
-        return servicePromise;
+            })
     }
-	
+
+    /**
+     * If we don't use a promise-oriented plugin, we have to build a promise by hand, which is messier
+     * although you can also be very detailed. It would look something like this (be warned -- this is
+     * UNTESTED CODE that probably doesn't work as is):
+    */
+    // getByCategory: function (category) {
+
+    //     var request = require('request');// This package uses callbacks instead of promises.
+
+    //     var options = {
+    //         uri: bookServiceEndpoint,
+    //         json: true
+    //     };
+
+    //     return new Promise(function (resolve, reject) {
+
+    //         request(options, function (error, response, body) {
+
+    //             if (error) {
+
+    //                 reject( new Error("Could not fetch books. " + error.message) )
+    //             }
+    //             else if (response.statusCode >= 400) {
+
+    //                 reject( new Error("Could not fetch books. " + body) );
+    //             }
+    //             else {
+
+    //                 var books = body;
+
+    //                 if ( books[category] ) {
+
+    //                     resolve( books[category] );
+    //                 }
+    //                 else {
+
+    //                     reject( new Error("No Books Found") );
+    //                 }
+    //             }
+    //         });
+    //     });
+    // }
+
 }
