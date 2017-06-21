111e050bc71b563dac6cfc18f495149fb08c7feb
Colin May <satsukikorin@gmail.com>
2017-06-21 16:09:59 -0700

    Simplified promises in books & category modules, and add catch blocks in router.

--- router.js
+++ router.js
@@ -1,118 +1,140 @@
-var categoryService = require("./services/categories.js"); 
-var bookService = require("./services/books.js"); 
+var categoryService = require("./services/categories.js");
+var bookService = require("./services/books.js");
 
 module.exports = function(app){
-     
+
      app.get('/',function(req,res){
-          
-          categoryService.get().then(function(categories){
-               
+
+          var categoricalPromise = categoryService.get();
+
+          Promise.resolve(categoricalPromise).then(function(categories){
+
               var viewModel = {
     	               category: "",
     	               categories: categories
     	          };
-    	          
+
         		res.render('index.html',viewModel);
-        		
+
+          })
+          .catch(function(err) {
+
+               console.error(err);
+        		res.render('index.html', {error: 'Sorry, there was an error. Please hit Back in your browser and try again.'});
           });
-          
+
      });
-     
-     
+
+
      app.get("/mystery", function(req, res) {
-                    
+
           var getCategories = categoryService.get();
           var getBooks = bookService.getByCategory("mystery");
-          
-          Promise.all([getCategories,getBooks]).then(function(values) {
-               
+
+          Promise.all([getCategories, getBooks]).then(function(values) {
+
                // "values" is an array, with each element in that array
                // being the object retuned by those 2 promises, in that
-               // order.  So, we can pull those out with "shift" and 
+               // order.  So, we can pull those out with "shift" and
                // get the things we want to work with...
-               
-               var categories = values.shift(); 
-               var books = values.shift(); 
-               
+
+               var categories = values.shift();
+               var books = values.shift();
+
     	          var viewModel = {
     	               categories: categories,
                     category:categories["mystery"],
     	               books: books
     	          };
-    	          
+
         		res.render('index.html',viewModel);
-        	});
-        	
+        	})
+          .catch(function(err) {
+
+               console.error(err);
+        		res.render('index.html', {error: 'Sorry, there was an error. Please hit Back in your browser and try again.'});
+          });
+
+
      });
-     
+
      app.get("/fantasy", function(req, res) {
-          
+
           var getCategories = categoryService.get();
           var getBooks = bookService.getByCategory("fantasy");
-          
+
           Promise.all([getCategories,getBooks]).then(function(values) {
-               
+
                // "values" is an array, with each element in that array
-               // being the object retuned by those 2 promises, in that
-               // order.  So, we can pull those out with "shift" and 
+               // being the object returned by those 2 promises, in that
+               // order.  So, we can pull those out with "shift" and
                // get the things we want to work with...
-               
-               var categories = values.shift(); 
-               var books = values.shift(); 
-               
+
+               var categories = values.shift();
+               var books = values.shift();
+
     	          var viewModel = {
     	               categories: categories,
                     category:categories["fantasy"],
     	               books: books
     	          };
-    	          
+
         		res.render('index.html',viewModel);
-        	});
-          
+        	})
+          .catch(function(err) {
+
+               console.error(err);
+        		res.render('index.html', {error: 'Sorry, there was an error. Please hit Back in your browser and try again.'});
+          });
+
      });
-     
+
      app.get("/children", function(req, res) {
           showCategory("children", req, res);
      });
-     
+
      app.get("/computing", function(req, res) {
           showCategory("computing", req, res);
      });
-     
+
      app.get("/memoir", function(req, res) {
           showCategory("memoir", req, res);
      });
-     
+
      // app.get('/:category', function (req, res) {
      //      var category = req.params.category;
      //      showCategory(category, req, res);
      // });
-     
-     
+
+
      function showCategory(category = "", req, res) {
-          
+
           var getCategories = categoryService.get();
           var getBooks = bookService.getByCategory(category);
-          
+
           Promise.all([getCategories,getBooks]).then(function(values) {
-               
+
                // "values" is an array, with each element in that array
                // being the object retuned by those 2 promises, in that
-               // order.  So, we can pull those out with "shift" and 
+               // order.  So, we can pull those out with "shift" and
                // get the things we want to work with...
-               
-               var categories = values.shift(); 
-               var books = values.shift(); 
-               
+
+               var categories = values.shift();
+               var books = values.shift();
+
     	          var viewModel = {
     	               categories: categories,
                     category:categories[category],
     	               books: books
     	          };
-    	          
+
         		res.render('index.html',viewModel);
-        	});
-          
+        	})
+          .catch(function(err) {
+
+               console.error(err);
+        		res.render('index.html', {error: 'Sorry, there was an error. Please hit Back in your browser and try again.'});
+          });
      };
-     
+
 };
